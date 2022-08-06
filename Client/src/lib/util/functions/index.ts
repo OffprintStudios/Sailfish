01e9c95cf8de7ed2e1ac82735ffc8e9d16d57046
Alyx <figments@outlook.com>
2022-08-06 09:55:04 -0700

    adding profile pages

--- Client/src/lib/util/functions/index.ts
+++ Client/src/lib/util/functions/index.ts
@@ -1,3 +1,7 @@
 export { copyToClipboard } from './copy-to-clipboard';
 export { intersection } from './intersection';
 export { clickOutside } from './click-outside';
+export { slugify } from './slugify';
+export { pluralize } from './pluralize';
+export { abbreviate } from './abbreviate';
+export { localeDate } from './locale-date';
