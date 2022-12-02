1d32cdd2cc7319699f88ad4b8a55537ede591aad
Alyx <figments@outlook.com>
2022-12-02 13:38:32 -0800

    Update +page.svelte

--- Client/src/routes/+page.svelte
+++ Client/src/routes/+page.svelte
@@ -34,23 +34,23 @@
 
 <div class="w-11/12 mx-auto max-w-7xl mb-6">
 	<div class="home-header">
-		<div class="w-1/4">
+		<div class="hidden lg:block w-1/4">
 			<div class="deco-box flex-col">
 				<span class="relative top-2 left-[0.075rem] text-4xl">{today.toLocaleString('default', { month: 'short' })}</span>
 				<span class="relative bottom-1.5 left-[0.075rem] text-xl">{today.getFullYear()}</span>
 			</div>
 		</div>
-		<div class="w-2/4 flex flex-col items-center justify-center">
+		<div class="lg:w-2/4 flex flex-col items-center justify-center">
 			<img
 				src="/images/logo.png"
 				alt="offprint logo"
-				class="max-w-[18rem] mx-auto"
+				class="max-w-[8rem] lg:max-w-[18rem] mx-auto"
 			/>
-			<h2 class="text-white font-light text-lg">
+			<h2 class="hidden lg:block text-white font-light text-lg">
 				{currSlogan}
 			</h2>
 		</div>
-		<div class="w-1/4 flex items-center justify-end">
+		<div class="hidden w-1/4 lg:flex items-center justify-end">
 			<div class="deco-box flex-row">
 				<span class="relative text-5xl font-normal">25</span>
 				<span class="relative text-3xl bottom-0.5 ml-1">¢</span>
@@ -61,7 +61,7 @@
 
 <style lang="scss">
 	div.home-header {
-		@apply text-white text-center my-6 rounded-xl px-4 py-2 w-full hidden lg:flex items-center justify-center;
+		@apply text-white text-center my-6 rounded-xl px-4 py-2 w-full flex items-center justify-center;
 		background: var(--accent);
 		div.deco-box {
 			@apply h-20 w-20 rounded-full flex items-center justify-center text-white font-semibold all-small-caps;
