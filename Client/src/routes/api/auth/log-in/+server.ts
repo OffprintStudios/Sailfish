4b6e94f58fbcc9e593a91edd59e81569269648f7
Alyx <figments@outlook.com>
2023-01-10 20:02:48 -0800

    force an update

--- Client/src/routes/api/auth/log-in/+server.ts
+++ Client/src/routes/api/auth/log-in/+server.ts
@@ -4,34 +4,35 @@ import type { ClientPackage } from "$lib/models/accounts";
 import type { LoginForm } from "$lib/models/accounts/forms";
 import { BASE_URL } from "$lib/http";
 
+// this better fucking work
 export const POST: RequestHandler = async ({ request, cookies, fetch }) => {
 	const formInfo: LoginForm = await request.json();
-	const userAgent = request.headers.get('User-Agent') ?? '';
+	const userAgent = request.headers.get("User-Agent") ?? "";
 	const response = await fetch(`${BASE_URL}/auth/login`, {
 		body: JSON.stringify(formInfo),
-		method: 'POST',
+		method: "POST",
 		headers: {
-			'User-Agent': userAgent,
-			'Content-Type': 'application/json',
+			"User-Agent": userAgent,
+			"Content-Type": "application/json"
 		}
 	});
 
 	if (response.status === 200) {
 		const data: ClientPackage = await response.json();
-		cookies.set('refreshToken', data.refreshToken, {
-			path: '/',
+		cookies.set("refreshToken", data.refreshToken, {
+			path: "/",
 			httpOnly: true,
-			expires: new Date(Date.now() + 2592000),
+			expires: new Date(Date.now() + 2592000)
 		});
-		data.refreshToken = '';
+		data.refreshToken = "";
 		return new Response(JSON.stringify(data), { status: 200 });
 	} else {
-		const error: { error: boolean, reason: string } = await response.json();
+		const error: { error: boolean; reason: string } = await response.json();
 		const errorMsg: ResponseError = {
 			statusCode: response.status,
 			error: response.statusText,
 			message: error.reason
-		}
+		};
 		return new Response(JSON.stringify(errorMsg), { status: response.status });
 	}
-}
+};
