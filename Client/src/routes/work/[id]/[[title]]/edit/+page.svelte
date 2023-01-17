c61aef0780cb4bc7a787b8fc9ca779d22d7890b8
Alyx <figments@outlook.com>
2023-01-17 08:43:04 -0800

    Update +page.svelte

--- Client/src/routes/work/[id]/[[title]]/edit/+page.svelte
+++ Client/src/routes/work/[id]/[[title]]/edit/+page.svelte
@@ -5,7 +5,7 @@
 	import { account } from "$lib/state/account.state";
 	import { QuillPenLine, Save2Line } from "svelte-remixicon";
 	import { Category, Status } from "$lib/models/content/works";
-	import { TextField, Editor } from "$lib/ui/forms";
+	import { TextField, Editor, TextArea } from "$lib/ui/forms";
 	import { getReq, patchReq, type ResponseError } from "$lib/http";
 	import toast from "svelte-french-toast";
 	import { goto } from "$app/navigation";
@@ -192,25 +192,25 @@
 
 {#if $account.account && $account.currProfile && $account.currProfile.id === work.author.id}
 	<form
-		class="my-6 max-w-4xl mx-auto rounded-xl overflow-hidden flex flex-col bg-zinc-200 dark:bg-zinc-700 dark:highlight-shadowed"
+		class="max-w-4xl mx-auto xl:rounded-xl overflow-hidden mb-6 xl:mt-6 flex flex-col bg-zinc-200 dark:bg-zinc-700 dark:highlight-shadowed"
 		use:form
 	>
 		<div class="flex items-center justify-center px-2 py-4" style="background: var(--accent);">
-			<QuillPenLine size="48px" class="text-white mr-2" />
-			<h1 class="text-white text-4xl">Editing "{work.title}"</h1>
+			<QuillPenLine class="text-white mr-2 w-[36px] h-[36px] lg:w-[48px] lg:h-[48px]" />
+			<h1 class="text-white text-2xl lg:text-4xl">Editing "{work.title}"</h1>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Category</h3>
-				<span class="text-xs italic text-zinc-400 text-right">
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
 					First thing's first. Is this an original work, or a fan work?
 				</span>
 			</div>
-			<div class="w-3/4 px-2.5 py-2.5 flex items-center justify-center">
+			<div class="w-full lg:w-3/4 px-2.5 py-2.5 flex items-center justify-center">
 				<div
-					class="flex flex-col p-4 bg-green-700 bg-opacity-10 rounded-xl border border-green-500 w-[250px] h-[111px]"
+					class="flex flex-col p-4 bg-green-700 bg-opacity-10 rounded-xl border border-green-500 w-1/2 lg:w-[250px] lg:h-[111px]"
 				>
 					<label class="mb-2">
 						<input type="radio" value={Category.Original} bind:group={$data.category} />
@@ -218,14 +218,14 @@
 							>Original Work</span
 						>
 					</label>
-					<div class="text-xs">
+					<div class="text-xs hidden lg:block">
 						Wholly original content&mdash;written, owned, and published by you, stories
 						yet untold.
 					</div>
 				</div>
 				<div class="mx-1"><!--spacer--></div>
 				<div
-					class="flex flex-col p-4 bg-cyan-700 bg-opacity-10 rounded-xl border border-cyan-500 w-[250px] h-[111px]"
+					class="flex flex-col p-4 bg-cyan-700 bg-opacity-10 rounded-xl border border-cyan-500 w-1/2 lg:w-[250px] lg:h-[111px]"
 				>
 					<label class="mb-2">
 						<input type="radio" value={Category.Fanwork} bind:group={$data.category} />
@@ -233,7 +233,7 @@
 							>Fan Work</span
 						>
 					</label>
-					<div class="text-xs">
+					<div class="text-xs hidden lg:block">
 						Derivative works&mdash;labors of love and naval combat, inspired by worlds
 						of other origin.
 					</div>
@@ -284,37 +284,40 @@
 		<div class="bg-zinc-300 dark:bg-zinc-600 border-b border-zinc-400 dark:border-zinc-500 p-3">
 			<h2 class="text-xl" style="color: var(--text-color);">General Info</h2>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Title</h3>
-				<span class="text-xs italic text-zinc-400 mb-1">120 characters max</span>
-				<span class="text-xs italic text-zinc-400 text-right">What's your work called?</span
-				>
+				<div class="flex-1 lg:hidden"><!--spacer--></div>
+				<span class="text-xs italic text-zinc-400 lg:mb-1">120 characters max</span>
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
+					What's your work called?
+				</span>
 			</div>
-			<div class="w-3/4 px-2.5 flex flex-col items-center justify-center">
+			<div class="lg:w-3/4 px-2.5 flex flex-col items-center justify-center">
 				<TextField
 					name="title"
 					type="text"
-					placeholder="Another Great Adventure"
+					placeholder="Just Another Rundown"
 					errorMessage={$errors.title}
 					kind="primary"
 				/>
 			</div>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Short Description</h3>
-				<span class="text-xs italic text-zinc-400 mb-1">240 characters max</span>
-				<span class="text-xs italic text-zinc-400 text-right"
+				<div class="flex-1 lg:hidden"><!--spacer--></div>
+				<span class="text-xs italic text-zinc-400 lg:mb-1">240 characters max</span>
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block"
 					>What's the basic idea of your work? Make it punchy!</span
 				>
 			</div>
-			<div class="w-3/4 px-2.5 flex flex-col items-center justify-center">
-				<TextField
+			<div class="w-full lg:w-3/4 px-2.5 flex flex-col items-center justify-center">
+				<TextArea
 					name="shortDesc"
 					type="text"
 					placeholder="A journey of a thousand words"
@@ -323,34 +326,37 @@
 				/>
 			</div>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Long Description</h3>
-				<span class="text-xs italic text-zinc-400 text-right">
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
 					Now set the stage&mdash;what's your work all about?
 				</span>
 			</div>
-			<div class="w-3/4 px-2.5 py-2.5">
+			<div class="w-full lg:w-3/4 px-2.5 py-2.5">
 				<Editor kind="primary" bind:value={$data.longDesc} />
 			</div>
 		</div>
 		<div class="bg-zinc-300 dark:bg-zinc-600 border-b border-zinc-400 dark:border-zinc-500 p-3">
 			<h2 class="text-xl" style="color: var(--text-color);">Tags</h2>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Genres</h3>
+				<div class="flex-1 lg:hidden"><!--spacer--></div>
 				<span class="text-xs italic text-zinc-400 mb-1">3 max, 1 minimum</span>
-				<span class="text-xs italic text-zinc-400 text-right">
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
 					What genres do you think apply here? Don't worry, you can always change this
 					later.
 				</span>
 			</div>
-			<div class="w-3/4 px-2.5 py-2.5 flex justify-center flex-wrap">
+			<div
+				class="w-full lg:w-3/4 px-2.5 py-2.5 flex items-center lg:justify-center flex-wrap"
+			>
 				{#each genres as genre}
 					<div
 						class="mx-[0.075rem] px-0.5 pb-1 rounded-lg border border-transparent"
@@ -366,19 +372,20 @@
 			</div>
 		</div>
 		{#if $data.category === Category.Fanwork}
-			<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+			<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 				<div
-					class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+					class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 				>
 					<h3 class="text-lg" style="color: var(--text-color);">Fandoms</h3>
+					<div class="flex-1 lg:hidden"><!--spacer--></div>
 					<span class="text-xs italic text-zinc-400 mb-1">1 minimum</span>
-					<span class="text-xs italic text-zinc-400 text-right">
+					<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
 						What worlds are you enhancing? Make sure to only include the most pertinent
 						ones!
 					</span>
 				</div>
 				<div
-					class="w-3/4 px-2.5 py-2.5 flex justify-center flex-wrap h-[250px] overflow-y-auto"
+					class="w-full lg:w-3/4 px-2.5 py-2.5 flex items-center lg:justify-center flex-wrap h-[250px] overflow-hidden overflow-y-auto"
 				>
 					{#if loadingFandoms}
 						<h5 class="font-medium text-lg">Loading...</h5>
@@ -402,19 +409,21 @@
 		<div class="bg-zinc-300 dark:bg-zinc-600 border-b border-zinc-400 dark:border-zinc-500 p-3">
 			<h2 class="text-xl" style="color: var(--text-color);">Metadata</h2>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Rating</h3>
-				<span class="text-xs italic text-zinc-400 text-right">
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
 					Check out the Offprint Omnibus for info about which one would be appropriate for
-					your work.
+					your blog.
 				</span>
 			</div>
-			<div class="w-3/4 px-2.5 py-2.5 flex items-center justify-center">
+			<div
+				class="w-full lg:w-3/4 px-2.5 py-2.5 grid grid-cols-2 lg:grid-cols-4 whitespace-nowrap lg:whitespace-normal"
+			>
 				<div
-					class="flex flex-col p-4 bg-red-700 bg-opacity-10 rounded-l-xl border border-red-500 w-[250px]"
+					class="flex flex-col p-4 bg-red-700 bg-opacity-10 rounded-tl-xl lg:rounded-l-xl border border-red-500"
 				>
 					<label class="mb-2">
 						<input
@@ -422,23 +431,23 @@
 							value={ContentRating.Everyone}
 							bind:group={$data.rating}
 						/>
-						<span class="font-bold uppercase ml-0.5 relative top-[0.075rem]"
+						<span class="font-bold uppercase ml-1 relative top-[0.075rem]"
 							>Everyone</span
 						>
 					</label>
-					<div class="text-xs">Not even a swear word.</div>
+					<div class="text-xs hidden lg:block">Not even a swear word.</div>
 				</div>
 				<div
-					class="flex flex-col p-4 bg-red-700 bg-opacity-10 border-y border-r border-red-500 w-[250px]"
+					class="flex flex-col p-4 bg-red-700 bg-opacity-10 rounded-tr-xl lg:rounded-tr-none border-y border-r border-red-500"
 				>
 					<label class="mb-2">
 						<input type="radio" value={ContentRating.Teen} bind:group={$data.rating} />
 						<span class="font-bold uppercase ml-0.5 relative top-[0.075rem]">Teen</span>
 					</label>
-					<div class="text-xs">Suitable for most works.</div>
+					<div class="text-xs hidden lg:block">Suitable for most works.</div>
 				</div>
 				<div
-					class="flex flex-col p-4 bg-red-700 bg-opacity-10 border-y border-red-500 w-[250px]"
+					class="flex flex-col p-4 bg-red-700 bg-opacity-10 border-t-0 border-b rounded-bl-xl lg:rounded-none lg:border-l-0 lg:border-y border-x border-red-500"
 				>
 					<label class="mb-2">
 						<input
@@ -450,10 +459,10 @@
 							>Mature</span
 						>
 					</label>
-					<div class="text-xs">Heavy topics, but nothing explicit.</div>
+					<div class="text-xs hidden lg:block">Heavy topics, but nothing explicit.</div>
 				</div>
 				<div
-					class="flex flex-col p-4 bg-red-700 bg-opacity-10 rounded-r-xl border border-red-500 w-[250px]"
+					class="flex flex-col p-4 bg-red-700 bg-opacity-10 rounded-br-xl lg:rounded-r-xl border-t-0 border lg:border-t border-l-0 border-red-500"
 				>
 					<label class="mb-2">
 						<input
@@ -465,22 +474,26 @@
 							>Explicit</span
 						>
 					</label>
-					<div class="text-xs">Like Mature, but goes into greater detail.</div>
+					<div class="text-xs hidden lg:block">
+						Like Mature, but goes into greater detail.
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="flex border-b border-zinc-400 dark:border-zinc-500">
+		<div class="flex flex-col lg:flex-row border-b border-zinc-400 dark:border-zinc-500">
 			<div
-				class="w-1/4 flex flex-col items-end px-4 pt-2 pb-3 border-r border-zinc-400 dark:border-zinc-500"
+				class="w-full lg:w-1/4 flex items-center lg:flex-col lg:items-end px-4 pt-2 pb-0 lg:pt-2 lg:pb-3 lg:border-r border-zinc-400 dark:border-zinc-500"
 			>
 				<h3 class="text-lg" style="color: var(--text-color);">Status</h3>
-				<span class="text-xs italic text-zinc-400 text-right">
+				<span class="text-xs italic text-zinc-400 text-right hidden lg:block">
 					What state is your work in? And no, I don't mean a US State.
 				</span>
 			</div>
-			<div class="w-3/4 px-2.5 py-2.5 flex items-center justify-center">
+			<div
+				class="w-full lg:w-3/4 px-2.5 py-2.5 grid grid-cols-2 lg:grid-cols-4 whitespace-nowrap lg:whitespace-normal"
+			>
 				<div
-					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 rounded-l-xl border border-teal-500 w-[250px]"
+					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 rounded-tl-xl lg:rounded-l-xl border border-teal-500 lg:w-auto"
 				>
 					<label class="mb-2">
 						<input type="radio" value={Status.Incomplete} bind:group={$data.status} />
@@ -488,10 +501,10 @@
 							>Incomplete</span
 						>
 					</label>
-					<div class="text-xs">A work in progress.</div>
+					<div class="text-xs hidden lg:block">A work in progress.</div>
 				</div>
 				<div
-					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 border-y border-teal-500 w-[250px]"
+					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 rounded-tr-xl lg:rounded-none border-r lg:border-r-0 border-y border-teal-500"
 				>
 					<label class="mb-2">
 						<input type="radio" value={Status.Complete} bind:group={$data.status} />
@@ -499,10 +512,10 @@
 							>Complete</span
 						>
 					</label>
-					<div class="text-xs">Totally wrapped up.</div>
+					<div class="text-xs hidden lg:block">Totally wrapped up.</div>
 				</div>
 				<div
-					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 border-y border-l border-teal-500 w-[250px]"
+					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 rounded-bl-xl lg:rounded-none border-t-0 border-b lg:border-y border-x lg:border-r-0 border-teal-500"
 				>
 					<label class="mb-2">
 						<input type="radio" value={Status.Paused} bind:group={$data.status} />
@@ -510,10 +523,10 @@
 							>Paused</span
 						>
 					</label>
-					<div class="text-xs">Taking a break.</div>
+					<div class="text-xs hidden lg:block">Taking a break.</div>
 				</div>
 				<div
-					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 rounded-r-xl border border-teal-500 w-[250px]"
+					class="flex flex-col p-4 bg-teal-700 bg-opacity-10 rounded-br-xl lg:rounded-r-xl border-b border-r lg:border border-teal-500"
 				>
 					<label class="mb-2">
 						<input type="radio" value={Status.Cancelled} bind:group={$data.status} />
@@ -521,7 +534,7 @@
 							>Cancelled</span
 						>
 					</label>
-					<div class="text-xs">Nothing more to say.</div>
+					<div class="text-xs hidden lg:block">Nothing more to say.</div>
 				</div>
 			</div>
 		</div>
