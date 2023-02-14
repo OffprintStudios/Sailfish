3a534a72e83c4d74e4978893afbf36ea72e1be3a
Alyx <figments@outlook.com>
2023-02-14 09:47:24 -0800

    feat: adding svelte query

--- Client/src/routes/+layout.svelte
+++ Client/src/routes/+layout.svelte
@@ -11,6 +11,8 @@
 	import { getReq, patchReq, type ResponseError } from "$lib/http";
 	import Button from "$lib/ui/util/Button.svelte";
 	import type { Account } from "$lib/models/accounts";
+	import { QueryClientProvider } from "@tanstack/svelte-query";
+	import { queryClient } from "$lib/http";
 
 	export let data: { token: string | null } = { token: null };
 	let loadingTerms = false;
@@ -60,63 +62,65 @@
 	}
 </script>
 
-<Popup />
-<main
-	class="flex flex-col min-h-[100vh] h-full overflow-y-scroll lg:overflow-y-hidden lg:flex-row lg:h-screen {$app.theme}"
-	class:light={$app.darkMode === false}
-	class:dark={$app.darkMode === true}
->
-	<Nav />
-	<Guide>
-		{#if $account.account && !$account.account.termsAgree}
-			<div
-				class="xl:max-w-4xl w-full flex flex-col lg:flex-row items-center mx-auto p-4 xl:mt-6 xl:rounded-xl bg-red-600 bg-opacity-25 border-y xl:border-x border-red-600"
-			>
-				<div class="flex-1">
-					<div class="flex items-center">
-						<CloseCircleLine size="18px" class="mr-2" />
-						<span class="font-bold text-lg">Head's Up!</span>
-					</div>
-					<span>
-						You haven't agreed to Offprint's Terms of Service, Privacy Policy, and Code
-						of Conduct. Until you do, all features of your account are locked. Hit the
-						"I Agree" button to fix this ASAP.
-					</span>
-				</div>
-				<Button on:click={agreeToTerms} loading={loadingTerms} loadingText="Saving...">
-					<CheckLine class="button-icon" />
-					<span class="button-text">I agree</span>
-				</Button>
-			</div>
-		{/if}
-		{#if $account.account && !$account.account.emailConfirmed}
-			<div
-				class="xl:max-w-4xl w-full flex flex-col lg:flex-row items-center mx-auto p-4 xl:mt-6 xl:rounded-xl bg-red-600 bg-opacity-25 border-y xl:border-x border-red-600"
-			>
-				<div class="flex-1">
-					<div class="flex items-center">
-						<CloseCircleLine size="18px" class="mr-2" />
-						<span class="font-bold text-lg">Head's Up!</span>
-					</div>
-					<span>
-						You haven't confirmed your email address yet. Until you do, all features of
-						your account are locked. Hit the "Confirm" button to fix this ASAP!
-					</span>
-				</div>
-				<Button
-					on:click={sendConfirmationEmail}
-					loading={loadingConfirm}
-					loadingText="Sending..."
+<QueryClientProvider client={queryClient}>
+	<Popup />
+	<main
+		class="flex flex-col min-h-[100vh] h-full overflow-y-scroll lg:overflow-y-hidden lg:flex-row lg:h-screen {$app.theme}"
+		class:light={$app.darkMode === false}
+		class:dark={$app.darkMode === true}
+	>
+		<Nav />
+		<Guide>
+			{#if $account.account && !$account.account.termsAgree}
+				<div
+					class="xl:max-w-4xl w-full flex flex-col lg:flex-row items-center mx-auto p-4 xl:mt-6 xl:rounded-xl bg-red-600 bg-opacity-25 border-y xl:border-x border-red-600"
 				>
-					<CheckLine class="button-icon" />
-					<span class="button-text">Confirm</span>
-				</Button>
-			</div>
-		{/if}
-		<slot />
-	</Guide>
-	<Toaster />
-</main>
+					<div class="flex-1">
+						<div class="flex items-center">
+							<CloseCircleLine size="18px" class="mr-2" />
+							<span class="font-bold text-lg">Head's Up!</span>
+						</div>
+						<span>
+							You haven't agreed to Offprint's Terms of Service, Privacy Policy, and
+							Code of Conduct. Until you do, all features of your account are locked.
+							Hit the "I Agree" button to fix this ASAP.
+						</span>
+					</div>
+					<Button on:click={agreeToTerms} loading={loadingTerms} loadingText="Saving...">
+						<CheckLine class="button-icon" />
+						<span class="button-text">I agree</span>
+					</Button>
+				</div>
+			{/if}
+			{#if $account.account && !$account.account.emailConfirmed}
+				<div
+					class="xl:max-w-4xl w-full flex flex-col lg:flex-row items-center mx-auto p-4 xl:mt-6 xl:rounded-xl bg-red-600 bg-opacity-25 border-y xl:border-x border-red-600"
+				>
+					<div class="flex-1">
+						<div class="flex items-center">
+							<CloseCircleLine size="18px" class="mr-2" />
+							<span class="font-bold text-lg">Head's Up!</span>
+						</div>
+						<span>
+							You haven't confirmed your email address yet. Until you do, all features
+							of your account are locked. Hit the "Confirm" button to fix this ASAP!
+						</span>
+					</div>
+					<Button
+						on:click={sendConfirmationEmail}
+						loading={loadingConfirm}
+						loadingText="Sending..."
+					>
+						<CheckLine class="button-icon" />
+						<span class="button-text">Confirm</span>
+					</Button>
+				</div>
+			{/if}
+			<slot />
+		</Guide>
+		<Toaster />
+	</main>
+</QueryClientProvider>
 
 <style lang="scss">
 	:global(main) {
