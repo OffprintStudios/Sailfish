0dbc09a714d2f8dd2fdf804b564e39d4007f9baf
Alyx <figments@outlook.com>
2023-07-23 15:08:41 -0700

    Update +page.svelte

--- Client/src/routes/social/+page.svelte
+++ Client/src/routes/social/+page.svelte
@@ -1,4 +1,9 @@
 <div class="empty">
-	<h3>Discover</h3>
-	<p>Discover</p>
+	<h3>Pardon the dust—</h3>
+	<p>
+		This feature is still under construction.<br />Keep an eye on our
+		<a href="https://github.com/OffprintStudios/Sailfish/releases" target="_blank"
+			>GitHub Releases</a
+		> for info on when this will become available!
+	</p>
 </div>
