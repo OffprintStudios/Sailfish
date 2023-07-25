3baf3ae38937e5ab7a74c6da7e2cec0bdfdc5fb5
Alyx <figments@outlook.com>
2023-07-25 08:44:15 -0700

    re-add confirmation boxes

--- Client/src/routes/+layout.svelte
+++ Client/src/routes/+layout.svelte
@@ -94,8 +94,66 @@
 		in:fly={{ easing: cubicOut, y: 10, delay: 400, duration: 300 }}
 		out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
 	>
-		<slot />
-		<div class="h-4 lg:hidden"><!--spacer--></div>
+		<div>
+			{#if $account.account && !$account.account.termsAgree}
+				<div
+					class="xl:max-w-4xl w-full flex flex-col lg:flex-row items-center mx-auto p-4 xl:mt-6 xl:rounded-xl bg-red-600 bg-opacity-25 border-y xl:border-x border-red-600"
+				>
+					<div class="flex-1">
+						<div class="flex items-center">
+							<CloseCircleLine size="18px" class="mr-2" />
+							<span class="font-bold text-lg">Head's Up!</span>
+						</div>
+						<span>
+							You haven't agreed to Offprint's Terms of Service, Privacy Policy, and
+							Code of Conduct. Until you do, all features of your account are locked.
+							Hit the "I Agree" button to fix this ASAP.
+						</span>
+					</div>
+					<Button on:click={agreeToTerms} loading={loadingTerms} loadingText="Saving...">
+						<CheckLine class="button-icon" />
+						<span class="button-text">I agree</span>
+					</Button>
+				</div>
+			{/if}
+			{#if $account.account && !$account.account.emailConfirmed}
+				<div
+					class="xl:max-w-4xl w-full flex flex-col lg:flex-row items-center mx-auto p-4 xl:mt-6 xl:rounded-xl bg-red-600 bg-opacity-25 border-y xl:border-x border-red-600"
+				>
+					<div class="flex-1">
+						<div class="flex items-center">
+							<CloseCircleLine size="18px" class="mr-2" />
+							<span class="font-bold text-lg">Head's Up!</span>
+						</div>
+						<span>
+							You haven't confirmed your email address yet. Until you do, all features
+							of your account are locked. Hit the "Confirm" button to fix this ASAP!
+						</span>
+					</div>
+					<Button
+						on:click={sendConfirmationEmail}
+						loading={loadingConfirm}
+						loadingText="Sending..."
+					>
+						<CheckLine class="button-icon" />
+						<span class="button-text">Confirm</span>
+					</Button>
+				</div>
+			{/if}
+			<slot />
+			<div class="h-4 lg:hidden"><!--spacer--></div>
+		</div>
 	</main>
 {/key}
 <Toaster />
+
+<style lang="scss">
+	main {
+		display: grid;
+		grid-template: 1fr / 1fr;
+	}
+
+	main > * {
+		grid-area: 1 / 1;
+	}
+</style>
