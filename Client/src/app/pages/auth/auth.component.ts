a3f4221ded9afbc2cc41ba4de921dbd336557f26
Alyx <figments@outlook.com>
2024-02-03 10:50:48 -0800

    adjust to appropriate formatting

--- Client/src/app/pages/auth/auth.component.ts
+++ Client/src/app/pages/auth/auth.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-auth',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+    selector: 'app-auth',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './auth.component.html',
+    styleUrl: './auth.component.css',
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
