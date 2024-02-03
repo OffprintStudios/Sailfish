5cd52902a1643a13480ea21a2681a87bc869db81
Alyx <figments@outlook.com>
2024-02-03 12:18:21 -0800

    menus now close on navigation

--- Client/src/app/ui/nav/search-menu/search-menu.component.ts
+++ Client/src/app/ui/nav/search-menu/search-menu.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, afterNextRender } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { NgIcon, provideIcons } from "@ng-icons/core";
 import { remixSearchEyeLine } from "@ng-icons/remixicon";
-import { NgxFloatUiModule } from "ngx-float-ui";
+import { NgxFloatUiContentComponent, NgxFloatUiModule } from "ngx-float-ui";
 import { DEFAULT_SLOGANS } from "$models/util";
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
@@ -18,8 +19,19 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
     styleUrl: './search-menu.component.css'
 })
 export class SearchMenuComponent {
+    @ViewChild(NgxFloatUiContentComponent) searchMenu!: NgxFloatUiContentComponent;
     currSlogan = DEFAULT_SLOGANS[Math.floor(Math.random() * DEFAULT_SLOGANS.length)];
     searchForm = new FormGroup({
         query: new FormControl(''),
     });
+
+    constructor(private router: Router) {
+        afterNextRender(() => {
+            this.router.events.subscribe((event) => {
+                if (event instanceof NavigationEnd) {
+                    this.searchMenu.hide();
+                }
+            });
+        });
+    }
 }
