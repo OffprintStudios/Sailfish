5dba0f301013caead99c7546f9f08c23772e02bd
Alyx <figments@outlook.com>
2024-02-24 10:22:39 -0800

    minor stuff and things

---
+++ Client/src/lib/state/app.state.ts
@@ -0,0 +1,91 @@
+import { browser } from "$app/environment";
+import { ModeSwitch, RatingsFilter, Themes } from "$lib/models/util";
+import { writable } from "svelte/store";
+
+interface AppState {
+    theme: Themes,
+    mode: ModeSwitch,
+    showNsfw: boolean,
+    filter: RatingsFilter,
+}
+
+const defaultAppState: AppState = {
+    theme: Themes.crimson,
+    mode: ModeSwitch.system,
+    showNsfw: false,
+    filter: RatingsFilter.restricted,
+};
+
+const initialAppState: AppState = browser
+    ? JSON.parse(window.localStorage.getItem("app") ?? "null") ?? defaultAppState
+    : defaultAppState;
+
+export const app = writable<AppState>(initialAppState);
+
+app.subscribe((value) => {
+    if (browser) {
+        window.localStorage.setItem("app", JSON.stringify(value));
+    }
+});
+
+export function setTheme(newTheme: Themes): void {
+    app.update((state) => {
+        const oldTheme = state.theme;
+        state.theme = newTheme;
+        const hasNewTheme = document.documentElement.classList.replace(oldTheme, state.theme);
+        if (hasNewTheme) {
+            const themeColor = document.querySelector("meta[name='theme-color']");
+            const accentColor = getComputedStyle(document.documentElement).getPropertyValue("--accent");
+            themeColor?.setAttribute("content", `rgba(${accentColor})`);
+        }
+        return state;
+    });
+}
+
+export function setMode(newMode: ModeSwitch): void {
+    app.update((state) => {
+        const modes = Object.keys(ModeSwitch);
+        document.body.classList.remove(...modes);
+        state.mode = newMode;
+        if (state.mode !== ModeSwitch.system) {
+            document.body.classList.add(state.mode);
+        } else {
+            if (document.defaultView!.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.body.classList.add(ModeSwitch.dark);
+            } else {
+                document.body.classList.add(ModeSwitch.light);
+            }
+        }
+        return state;
+    });
+}
+
+export function setShowNsfw(): void {
+    app.update((state) => ({
+        ...state,
+        showNsfw: !state.showNsfw,
+    }));
+}
+
+export function setFilter(enableMature: boolean, enableExplicit: boolean): void {
+    app.update((state) => ({
+        ...state,
+        filter: determineRatingsFilter(enableMature, enableExplicit),
+    }));
+}
+
+function determineRatingsFilter(enableMature: boolean, enableExplicit: boolean): RatingsFilter {
+	let filterSetting: RatingsFilter = RatingsFilter.restricted;
+
+	if (enableMature === true && enableExplicit === false) {
+		filterSetting = RatingsFilter.mature;
+	} else if (enableMature === false && enableExplicit === true) {
+		filterSetting = RatingsFilter.explicit;
+	} else if (enableMature === true && enableExplicit === true) {
+		filterSetting = RatingsFilter.everything;
+	} else if (enableMature === false && enableExplicit === false) {
+		filterSetting = RatingsFilter.restricted;
+	}
+
+	return filterSetting;
+}
---
+++ Client/src/lib/ui/nav/Navbar.svelte
@@ -0,0 +1,92 @@
+<div class="navbar-container">
+    <nav class="flex items-center max-w-[90rem] w-full md:w-11/12 pl-4 pr-2 md:px-0 mx-auto h-full">
+        <div class="flex items-center w-1/3 text-white">
+            <a href="/"><h3 class="text-2xl font-bold tracking-tighter text-white">Offprint</h3></a>
+            <a class="page-link" href="/explore">Explore</a>
+            <a class="page-link" href="/social">Social</a>
+        </div>
+        <div class="w-1/3">
+            <!--<app-search-menu />-->
+        </div>
+        <div class="flex items-center w-1/3" style="font-family: var(--header-text);">
+            <div class="flex-1"><!--spacer--></div>
+            <!--@if (isAuthenticated$ | async) {
+                @if (currProfile$ | async; as profile) {
+                    <button class="navbar-button">
+                        <span class="icon"><ng-icon name="remixInbox2Line" size="24px" /></span>
+                        <span class="text">1.2k</span>
+                    </button>
+                    <button class="navbar-button">
+                        <span class="icon"><ng-icon name="lucideLibrary" size="24px" strokeWidth="2.5" /></span>
+                        <span class="text">12</span>
+                    </button>
+                    <app-user-menu />
+                } @else {
+                    <a class="navbar-button" [routerLink]="['/auth/switch-profile']">
+                        <span class="icon"><ng-icon name="lucideArrowRightLeft" size="18px" /></span>
+                        <span class="link-text">Profiles</span>
+                    </a>
+                }
+            } @else {
+                <a class="navbar-button" [routerLink]="['/auth/log-in']">
+                    <span class="icon"><ng-icon name="lucideLogIn" size="18px" /></span>
+                    <span class="link-text">Log In</span>
+                </a>
+            }-->
+            <a class="navbar-button" href="/auth/log-in">
+                <span class="icon"><ng-icon name="lucideLogIn" size="18px" /></span>
+                <span class="link-text">Log In</span>
+            </a>
+        </div>
+    </nav>
+</div>
+
+<style lang="scss">
+    div.navbar-container {
+        @apply sticky top-0 w-full h-[55px] md:h-[65px] z-50 drop-shadow-2xl border-b border-white border-opacity-25 backdrop-blur-lg;
+        background-color: rgb(var(--accent));
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+
+        &.not-home {
+            @apply border-b-transparent;
+            /* background-color: rgba(var(--accent), 0.80);*/
+        }
+
+        button.navbar-button, a.navbar-button {
+            @apply hidden md:flex items-center text-white mx-0.5 px-2 pb-0.5 rounded-xl transition;
+
+            & > span {
+                @apply relative;
+            }
+
+            & > span.icon {
+                @apply top-1;
+            }
+
+            & > span.text {
+                @apply top-0.5 ml-1.5 hidden lg:block;
+            }
+
+            &:hover {
+                @apply bg-zinc-300 bg-opacity-25 backdrop-blur;
+            }
+        }
+
+        a.navbar-button {
+            @apply flex px-3 py-1;
+
+            & > span.link-text {
+                @apply hidden md:block all-small-caps font-bold tracking-wider ml-1.5;
+            }
+        }
+
+        a.page-link {
+            @apply hidden md:block relative top-0.5 text-white ml-4 all-small-caps text-lg border-b-2 border-transparent transition font-semibold;
+            font-family: var(--header-text);
+
+            &.active {
+                @apply border-zinc-300 border-opacity-50;
+            }
+        }
+    }
+</style>
