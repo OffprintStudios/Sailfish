22b94aa4dcfcc8163387de322032f76e29102244
Alyx <figments@outlook.com>
2024-04-22 21:26:29 -0700

    update packages

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.92.8"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.93.0"),
         // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
         // 🐘 Fluent driver for Postgres.
