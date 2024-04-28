3c029a44edfa12716ed89ee966ec7581c5c013a4
Figments <figments@outlook.com>
2024-04-28 13:53:36 -0700

    add surrealdb-migrations

--- src/models/accounts/account.rs
+++ src/models/accounts/account.rs
@@ -1,4 +1,4 @@
-use argon2::{Argon2, password_hash::{rand_core::OsRng, SaltString}, PasswordHasher};
+// use argon2::{Argon2, password_hash::{rand_core::OsRng, SaltString}, PasswordHasher};
 
 pub struct Account {
     pub id: String,
@@ -11,30 +11,23 @@ pub struct Account {
 }
 
 impl Account {
-    pub fn init(form_info: RegisterForm) -> Self {
+    pub fn init(email: String, password: String) -> Self {
         Account {
             id: "uuid".to_string(),
-            email: form_info.email,
-            password: Self::hash_password(form_info.password),
-            terms_agree: form_info.terms_agree,
+            email,
+            password,
+            terms_agree: true,
             email_confirmed: false,
             created_at: Default::default(),
             updated_at: Default::default(),
         }
     }
     
-    fn hash_password(password: String) -> String {
+    /*fn hash_password(password: String) -> String {
         let argon2 = Argon2::default();
         let pw = password.as_bytes();
         let salt = SaltString::generate(&mut OsRng);
         let hash = argon2.hash_password(pw, &salt);
         hash.unwrap().to_string() // TODO: handle potential errors better
-    }
-}
-
-pub struct RegisterForm {
-    pub email: String,
-    pub password: String,
-    pub age_check: bool,
-    pub terms_agree: bool,
+    }*/
 }
