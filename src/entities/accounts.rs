3001cb3c9e0a6d3da2c908928decf6a7a76af09e
Alyx <figments@outlook.com>
2024-04-29 23:24:33 -0700

    add session entity

--- src/entities/accounts.rs
+++ src/entities/accounts.rs
@@ -18,7 +18,16 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::sessions::Entity")]
+    Sessions,
+}
+
+impl Related<super::sessions::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Sessions.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
 
--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -3,3 +3,4 @@
 pub mod prelude;
 
 pub mod accounts;
+pub mod sessions;
