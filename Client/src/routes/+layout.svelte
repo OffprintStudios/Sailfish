7a5140612e734d32614bd61fb9ef92e7522babbb
Alyx Mote <figments@outlook.com>
2024-11-25 20:27:48 -0800

    removing localization

--- Client/src/routes/+layout.svelte
+++ Client/src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { i18n } from '$lib/i18n';
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-<ParaglideJS {i18n}>
-	{@render children()}
-</ParaglideJS>
+{@render children()}
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,15 +1,8 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		paraglide({
-			project: './project.inlang',
-			outdir: './src/lib/paraglide'
-		})
-	],
+	plugins: [sveltekit()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
