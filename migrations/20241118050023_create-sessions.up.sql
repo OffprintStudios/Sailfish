e39f62a7b4764fcfde9d19086753567da59a640d
Alyx Mote <figments@outlook.com>
2025-03-02 18:53:15 -0800

    fix issues with foreign keys

--- migrations/20241118050023_create-sessions.up.sql
+++ migrations/20241118050023_create-sessions.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS sessions (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    account_id VARCHAR(21) NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
+    account_id UUID NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
     ip_addr TEXT DEFAULT NULL,
     browser TEXT DEFAULT NULL,
     device TEXT DEFAULT NULL,
--- migrations/20241211152105_create-otp.up.sql
+++ migrations/20241211152105_create-otp.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS otp (
     id VARCHAR(21) PRIMARY KEY DEFAULT nanoid(),
-    account_id VARCHAR(21) NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
+    account_id UUID NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
     token VARCHAR(21) NOT NULL DEFAULT nanoid(),
     kind TEXT NOT NULL,
     expires_on TIMESTAMPTZ NOT NULL DEFAULT (current_timestamp + interval '1' hour),
